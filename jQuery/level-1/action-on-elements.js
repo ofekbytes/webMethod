24610215865a4bdeb76b7a6b463de5c5db09365f
Yaron Kessler <Yaron.Kessler@brinks.com>
2018-08-14 13:43:20 +0300

    example

---
+++ jQuery/level-1/action-on-elements.js
@@ -0,0 +1,31 @@
+
+-=[ Action on Element ]=-
+
+// all element
+$("*")
+
+// element (id = idElement()
+$("#idElement")
+
+// all element with (class = classElement)
+$(".classElement")
+
+// all <p> element/s
+$("p")
+
+// all <h1> <h2> <h3> element/s
+$("h1,h2,h3")
+
+// ":first" <h1> element
+$("h1:first")
+
+// ":last" <h2> element
+$("h2:last")
+
+// ":first-child" - all <b> element that are the first child of their parent
+$("b:first-child")
+
+
+
+
+
